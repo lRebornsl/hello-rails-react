cc8a58318874a83221e39a342f226c6ecf68d2be
lRebornsl <anthony_vasquez98@hotmail.com>
2023-11-06 09:26:59 -0500

    Create models and migrations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,5 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Message.create(content: "Hello, welcome to GreetingsApp!")
+Message.create(content: "Hi there! Enjoy your day.")
+Message.create(content: "Greetings! How can I assist you?")
+Message.create(content: "Hey, nice to see you!")
+Message.create(content: "Welcome! Have a great time!")
