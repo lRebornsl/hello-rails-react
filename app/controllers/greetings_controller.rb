d18525fea801590a724c29ee28e3e0245f91f884
lRebornsl <anthony_vasquez98@hotmail.com>
2023-11-07 10:31:21 -0500

    Fix liners

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # Controller
 class GreetingsController < ApplicationController
   def random_greeting
--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # Controller
 class RootController < ApplicationController
   def index; end
